3cfe1f192984655948a6b947af2450ef64d8c976
osvaldo345 <42743246+osvaldo345@users.noreply.github.com>
2019-05-20 23:00:00 -0500

    se creo el main

---
+++ agendita/main.js
@@ -0,0 +1,31 @@
+import Tabla from "./Tabla.js";
+import Contactos from "./Contactos.js";
+
+class Main {
+    constructor() {
+        this._agenda = new Tabla(document.querySelector("#agenda"));
+
+        document.querySelector("#btnAdd").addEventListener("click",() => {
+            let name = document.querySelector("#name").value;
+            let cel =document.querySelector("#cel").value;
+            let sBirthday = document.querySelector("#birthday").value;
+            sBirthday = sBirthday.split('-');
+
+        
+            let birthday = new Date(sBirthday[0], sBirthday[1], sBirthday[2]);
+
+            let correo =document.querySelector("#correo").value;
+
+
+
+
+            let Contacto = new Contactos(name, cel, birthday, correo);
+
+            this._agenda.addContacto(Contacto);
+        });
+    }
+
+
+}
+
+new Main(); 
